218f36ad00d76d615f45fe68dba9fbc0cd477073
Hakhaz <48715812+Hakhaz@users.noreply.github.com>
2019-03-22 19:00:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def trade_spider(max_pages):
         print url
         source_code = requests.get(url)
         plain_text = source_code.text
-        soup = BeautifulSoup(plain_text)
+        soup = BeautifulSoup(plain_text) #Em mostra aquesta línia per pantalla, no entenc el perquè, o si es normal o no
         for link in soup.findAll('a', {'class': 'cover'}):
             href = link.get('href')
-            title = link.string
+            #title = link.string
             print(href)
-            print(title)
-            #get_single_item_data(href)
+            #print(title)
+            get_single_item_data(href)
         page+=1
 
 def get_single_item_data(item_url):
@@ -23,8 +23,14 @@ def get_single_item_data(item_url):
     soup = BeautifulSoup(plain_text)
     for item_name in soup.findAll('h1'):
         print(item_name.string)
-    #for item_price in soup.findAll('div', {'class':'prices'}):
-        #print(item_price)
+    for item_prices in soup.findAll('div', {'class':'prices'}):
+        retail = item_prices.find('span')
+        price = item_prices.find('div',{'class':'price'})
+        discount = item_prices.find('div',{'class':'discount'})
+        print(retail.string)
+        print(price.string)
+        print(discount.string)
 
-trade_spider(1)
-#get_single_item_data('https://www.instant-gaming.com/es/2467-comprar-key-uplay-the-division-2/')
+#trade_spider(1)
+#get_single_item_data('https://www.instant-gaming.com/es/186-comprar-key-rockstar-grand-theft-auto-v/')
+get_single_item_data('https://www.instant-gaming.com/es/2467-comprar-key-uplay-the-division-2/')
