629de1c756b138d1115e04d4d0737373b93f5dac
vodskar2 <48836836+vodskar2@users.noreply.github.com>
2019-03-23 21:38:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import pandas as pd
 
 def trade_spider(max_pages):
-    page=1
+    page=1    
+    df = pd.DataFrame()
+    #columns = ['name','platform','original_price','price','discount','release_data','users_rating','category']
     while page <= max_pages:
         url = 'https://www.instant-gaming.com/es/busquedas/?q=&page=' + str(page)
         print (url)
@@ -12,13 +15,18 @@ def trade_spider(max_pages):
         soup = BeautifulSoup(plain_text) #Em mostra aquesta línia per pantalla, no entenc el perquè, o si es normal o no
         for link in soup.findAll('a', {'class': 'cover'}):
             href = link.get('href')
-            #title = link.string
             print(href)
-            #print(title)
             category = link.parent.get('data-dlc') #0 = jocs, 1 = dlc
-            print('category: {}'.format(category))
-            get_single_item_data(href)
+            #print('category: {}'.format(category))
+            
+            data = get_single_item_data(href)
+            data.append(category)
+            print(data)
+
+            df = df.append(pd.DataFrame([data]),ignore_index=True) #amb [[data]] ho posa com una fila. Poc elegant pero funciona.
         page+=1
+        
+        df.to_csv('data.csv', sep=',')
 
 def get_single_item_data(item_url):
     source_code = requests.get(item_url)
@@ -26,14 +34,11 @@ def get_single_item_data(item_url):
     soup = BeautifulSoup(plain_text)
     
     title = soup.find('div',{'class':'title'}).h1.get_text()
-    print(title)
-    #platform = soup.find('div',{'class':'subinfos'}).a.get('class')
     platform = soup.find('div',{'class':'subinfos'}).a.get_text()
-    print(platform)
+    #print(title)
+    #print(platform)
     
-    #alternativa sense bucle, ja es buscaran els DLCs quan els hi toqui
     item_prices = soup.find('div', {'class':'prices'})      
-    #for item_prices in soup.findAll('div', {'class':'prices'}):
     try:
         retail = item_prices.find('span').string
     except:
@@ -44,33 +49,31 @@ def get_single_item_data(item_url):
         discount = item_prices.find('div',{'class':'discount'}).string
     except:
         discount = ''
-    print(retail)
-    print(price)
-    print(discount)
+    #print(retail)
+    #print(price)
+    #print(discount)       
        
-       
-    release_date = soup.find('div', {'class':'release'})
-    print(release_date.string)
-    
+    release_date = soup.find('div', {'class':'release'}).contents[3].get_text()
     users_rating = soup.find('div', {'class':'productreviews'}).contents[1].span.get_text()
-    print(users_rating)
+    #print(release_date)
+    #print(users_rating)
     
-    data = [title, platform]
-    writer.writerow(data)
+    #data = [title, platform]
+    #writer.writerow(data)
     
-    #for item_name in soup.findAll('div', {'class':'stars'}):        
-    #    print(item_name)
-
+    data = [title,platform,retail,price,discount,release_date,users_rating]
+    return (data)
+    
 #Exemple per a probar l'escriptura al csv de la primera pàgina
-with open('data.csv', 'w') as csvFile:
-    writer = csv.writer(csvFile, delimiter=',')
-    trade_spider(1)
+#with open('data.csv', 'w') as csvFile:
+#    writer = csv.writer(csvFile, delimiter=',')
+trade_spider(1)
 #get_single_item_data('https://www.instant-gaming.com/es/2467-comprar-key-uplay-the-division-2/')
 #get_single_item_data('https://www.instant-gaming.com/es/186-comprar-key-rockstar-grand-theft-auto-v/')
 
 #Exemple per a probar l'escriptura al csv de únicament tres jocs
-with open('data.csv', 'w') as csvFile:
-    writer = csv.writer(csvFile, delimiter=',')
-    get_single_item_data('https://www.instant-gaming.com/es/2467-comprar-key-uplay-the-division-2/')
-    get_single_item_data('https://www.instant-gaming.com/es/186-comprar-key-rockstar-grand-theft-auto-v/')
-    get_single_item_data('https://www.instant-gaming.com/es/3979-comprar-key-origin-apex-legends-2150-apex-coins/')
+#with open('data.csv', 'w') as csvFile:
+#    writer = csv.writer(csvFile, delimiter=',')
+#    get_single_item_data('https://www.instant-gaming.com/es/2467-comprar-key-uplay-the-division-2/')
+#    get_single_item_data('https://www.instant-gaming.com/es/186-comprar-key-rockstar-grand-theft-auto-v/')
+#    get_single_item_data('https://www.instant-gaming.com/es/3979-comprar-key-origin-apex-legends-2150-apex-coins/')
