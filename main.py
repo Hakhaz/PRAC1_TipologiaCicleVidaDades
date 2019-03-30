ea9c9d4fc070679ca34cd702a51bdb35e50b0b80
Hakhaz <48715812+Hakhaz@users.noreply.github.com>
2019-03-30 19:54:12 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,95 +1,11 @@
-import requests
-from bs4 import BeautifulSoup
-import csv
+from scraper import get_max_pages
+from scraper import trade_spider
+from scraper import df_to_csv
 import pandas as pd
-import datetime
-
-def get_max_pages():
-    url = 'https://www.instant-gaming.com/es/busquedas/?q=&page=1'
-    source_code = requests.get(url)
-    plain_text = source_code.text
-    soup = BeautifulSoup(plain_text,'lxml')
-    pagination = soup.find('ul', {'class':'pagination bottom'})
-    pageMax = 1
-    for pages in pagination.findAll('li'):
-        try:
-            page=int(pages.string)
-        except ValueError:
-            page=-1
-        if page > pageMax:
-            pageMax = page
-    return(pageMax)
 
 
+max_pages = get_max_pages()
+df = pd.DataFrame()
 
-def trade_spider(max_pages):
-    print('Start scraper. Total pages:',max_pages)
-    page=1
-    df = pd.DataFrame()
-    while page <= max_pages:
-        url = 'https://www.instant-gaming.com/es/busquedas/?q=&page=' + str(page)
-        print('Start scraper page',page)
-
-        source_code = requests.get(url)
-        plain_text = source_code.text
-        soup = BeautifulSoup(plain_text,'lxml')
-        for link in soup.findAll('a', {'class': 'cover'}):
-            href = link.get('href')
-            
-
-            if link.parent.get('data-dlc') == '0':
-                category = 'GAME'
-            elif link.parent.get('data-dlc') == '1':
-                category = 'DLC'
-            else:
-                category = 'UNKNOWN'
-                
-            
-            data = get_single_item_data(href)
-            data.append(category)
-            
-            now = datetime.datetime.now()
-            dateExtraction = now.strftime("%Y-%m-%d")
-            data.append(dateExtraction)
-
-
-            df = df.append(pd.DataFrame([data]),ignore_index=True)
-        print('End scraper page',page)
-        page+=1
-        
-    columns = ['name','platform','original_price','discounted_price','discount','release_data','users_rating','category','extraction_date']   
-    now = datetime.datetime.now()
-    fileName = 'GamesDataPrices_'+now.strftime("%Y%m%d_%H%M")+'.csv'
-    print(fileName)
-    df.to_csv(fileName, sep=',', header=columns)
-    print('End scraper')
-    
-def get_single_item_data(item_url):
-    source_code = requests.get(item_url)
-    plain_text = source_code.text
-    soup = BeautifulSoup(plain_text,'lxml')
-    
-    title = soup.find('div',{'class':'title'}).h1.get_text()
-    platform = soup.find('div',{'class':'subinfos'}).a.get_text()
-    
-    item_prices = soup.find('div', {'class':'prices'})      
-    try:
-        retail = item_prices.find('span').string
-    except:
-        retail = ''
-    price = item_prices.find('div',{'class':'price'}).string
-    
-    try:
-        discount = item_prices.find('div',{'class':'discount'}).string
-    except:
-        discount = ''
-     
-       
-    release_date = soup.find('div', {'class':'release'}).contents[3].get_text()
-    users_rating = soup.find('div', {'class':'productreviews'}).contents[1].span.get_text()
-
-    data = [title,platform,retail,price,discount,release_date,users_rating]
-    return (data)
-
-trade_spider(get_max_pages())
-
+df = trade_spider(1)
+df_to_csv(df)
