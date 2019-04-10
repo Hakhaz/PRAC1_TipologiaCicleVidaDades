1bb4400dd8ee89c762d14b95d3099223d4ef762a
Hakhaz <48715812+Hakhaz@users.noreply.github.com>
2019-04-10 19:07:16 +0200

    Update main.py

--- scraper/main.py
+++ scraper/main.py
@@ -3,9 +3,11 @@ from scraper import trade_spider
 from scraper import df_to_csv
 import pandas as pd
 
-
+#Primer s'obtè el nombre màxim de pàgines que s'ha de recòrrrer
 max_pages = get_max_pages()
+#Es crea el dataframe
 df = pd.DataFrame()
-
+#Es truca a la funció trade_spider per obtenir el dataframe que contè tota la informació
 df = trade_spider(max_pages)
+#Es truca a la funció df_to_csv per guardar la informació del dataframe en un arxiu csv
 df_to_csv(df)
